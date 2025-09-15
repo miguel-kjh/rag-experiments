37b8322ccb0d9f8ced190bb9d4a588c5423deed2
miguel-kjh <miguelangel.kjh@gmail.com>
2025-09-15 10:36:16 +0100

    Add data management utilities and dataset download functionality

---
+++ src/utils.py
@@ -0,0 +1,6 @@
+import os
+
+FOLDER_DATA      = "data"
+FOLDER_RAW       = os.path.join(FOLDER_DATA, "raw")
+FOLDER_PROCESSED = os.path.join(FOLDER_DATA, "processed")
+FOLDER_COMBINED  = os.path.join(FOLDER_DATA, "combined")
