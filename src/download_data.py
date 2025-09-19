5837467e5467255e6d2e86430ba2ff7eff877202
miguel-kjh <miguelangel.kjh@gmail.com>
2025-09-19 11:30:25 +0100

    Enhance RAG-Bench dataset download function to retain specified columns, add document IDs, and save unique documents to a pickle file

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 import argparse
+import pickle
 import os
 
 from utils import (
@@ -20,15 +21,33 @@ def download_ragbench():
     for subset in RANGBENCH_SUBSETS:
         print(f"Downloading RAG-Bench subset: {subset}")
         dataset = load_dataset("rungalileo/ragbench", subset)
+        documents = []
+        for split in dataset:
+            for docs in dataset[split]["documents"]:
+                for doc in docs:
+                    documents.append(doc)
+        unique_documents = list(set(documents))
+        unique_documents.sort()
+        print(f"Number of unique documents in {subset}: {len(unique_documents)}")
+        document_idx_map = {doc: idx for idx, doc in enumerate(unique_documents)}
         # Keep only specified columns in each split
         for split in dataset.keys():
             dataset[split] = dataset[split].remove_columns(
                 [col for col in dataset[split].column_names if col not in columns_to_keep]
             )
+            dataset[split] = dataset[split].add_column(
+                "document_ids",
+                [[document_idx_map[doc] for doc in docs] for docs in dataset[split]["documents"]]
+            )
+        print(f"Columns in {subset} after processing: {dataset['train'].column_names}")
         # Save the entire dataset (all splits) to disk
         folder_path = os.path.join(FOLDER_RAW, f"ragbench-{subset}")
         dataset.save_to_disk(folder_path)
         print(f"RAG-Bench subset saved to {folder_path}")
+        # save unique documents to a pkl file
+        with open(os.path.join(folder_path, "unique_documents.pkl"), "wb") as f:
+            pickle.dump(unique_documents, f)
+        print(f"Unique documents saved to {os.path.join(folder_path, 'unique_documents.pkl')}")
 
 DATASETS_TO_DOWNLOAD = {
     "ragbench": download_ragbench,
