40c7d15bdf7b91563c2577e14d5b31ba19d0e2fe
miguel-kjh <miguelangel.kjh@gmail.com>
2025-09-24 11:28:41 +0100

    Refactor evaluation functions to streamline dataset loading and integrate re-identification metrics evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,15 +1,15 @@
 import os
 import openai
 import pandas as pd
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from ragas import evaluate, EvaluationDataset
 from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
 from ragas.embeddings import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 
+from reid_metrics import calc_reid_metrics
 from utils import SEED
-
 
 # -----------------------------
 # Configuration
@@ -27,26 +27,32 @@ def setup_environment():
 
 
 def build_components():
-    """Initialize evaluation components (LLM, embeddings)."""
-    evaluator_llm = ChatOpenAI(model=MODEL_NAME, seed=SEED, temperature=0.0)
-    openai_client = openai.OpenAI()
-    embeddings = OpenAIEmbeddings(client=openai_client)
-    return evaluator_llm, embeddings
+    """Initialize evaluation components"""
+    evaluator_llm = ChatOpenAI(model=MODEL_NAME, seed=SEED)
+    return evaluator_llm
 
 
 def load_dataset(path: str, subsample: int = None):
     """Load dataset from JSONL and optionally subsample it."""
-    dataset = EvaluationDataset.from_jsonl(path)
+    #dataset = EvaluationDataset.from_jsonl(path)
+    dataset = pd.read_json(path, lines=True).to_dict(orient="records")
     if subsample:
         dataset = dataset[:subsample]
     print(f"Loaded dataset with {len(dataset)} samples.")
     return dataset
 
 
-def run_evaluation(dataset, evaluator_llm):
+def run_llm_evaluation(dataset, evaluator_llm):
     """Run evaluation on the dataset using predefined metrics."""
+    ragas_dataset = EvaluationDataset.from_list(dataset)
     metrics = [LLMContextRecall(), Faithfulness(), FactualCorrectness()]
-    return evaluate(dataset=dataset, metrics=metrics, llm=evaluator_llm)
+    return evaluate(dataset=ragas_dataset, metrics=metrics, llm=evaluator_llm)
+
+def run_reid_evaluation(dataset):
+    """Run re-identification metrics evaluation."""
+    preds = [item["document_ids"] for item in dataset]
+    gts = [item["target_document_ids"] for item in dataset]
+    return calc_reid_metrics(preds, gts)
 
 
 def save_results(result, output_path: str):
@@ -61,8 +67,17 @@ def main():
     setup_environment()
     evaluator_llm, _ = build_components()
     dataset = load_dataset(DATASET_PATH, SUBSAMPLE_SIZE)
-    result = run_evaluation(dataset, evaluator_llm)
-    save_results(result, OUTPUT_PATH)
+    reid_result = run_reid_evaluation(dataset)
+    llm_result = run_llm_evaluation(dataset, evaluator_llm)
+    score_strs = {k: round(v, 4) for k, v in llm_result._repr_dict.items()}
+    # join both results
+    final_result = score_strs.copy()
+    for k, v in reid_result.items():
+        final_result[k] = round(v, 4)
+    print(final_result)
+    exit()
+    save_results(llm_result, OUTPUT_PATH)
+
 
 
 if __name__ == "__main__":
